72ad822c6260be2ca85ef9c3f231552f91227b75
Matthew Loseke <mloseke@gmail.com>
2014-07-17 13:54:59 -0500

    wip save smsCode in db

--- cloud/twilio.js
+++ cloud/twilio.js
@@ -1,20 +1,9 @@
 var code = [];
 var message;
+var SMSCode = Parse.Object.extend("SMSCode");
+
 var twilio = require("twilio");
 twilio.initialize("ACe7c2b91203350d82da8e09596e07959d","c77d656ed20c198c5e938a2f46b5037b");
-
-exports.sendSMSVerification = function(request, response) {
-  generateCode();
-  codeMessage();
-  twilio.sendSMS({
-    From: "+17082776170",
-    To: request.params.number,
-    Body: message
-  }, {
-    success: function(httpResponse) { response.success("SMS sent!"); },
-    error: function(httpResponse) { response.error("Uh oh, something went wrong"); }
-  });
-}
 
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -24,8 +13,35 @@ function generateCode() {
     for (var i = 0; i < 6; i++) {
         code[i] = randomIntFromInterval(1, 9);
     }
+    code = code.join("");
 }
 
 function codeMessage() {
-    message = "Your code is " + code.join("");
+    message = "Your code is " + code;
+}
+
+exports.sendSMSVerification = function(request, response) {
+  generateCode();
+  codeMessage();
+  twilio.sendSMS({
+    From: "+17082776170",
+    To: request.params.number,
+    Body: message
+  }, {
+    success: function(httpResponse) {
+      response.success("SMS sent!");
+      var smsCode = new SMSCode();
+      smsCode.set("code", code);
+      smsCode.set("verified", false);
+      smsCode.save(null, {
+        success: function(smsCode) {
+          // we saved it
+        },
+        error: function(smsCode, error) {
+          // nope
+        }
+      });
+    },
+    error: function(httpResponse) { response.error("Uh oh, something went wrong"); }
+  });
 }
