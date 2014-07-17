2e559ba816b251b190971fa4e9996b4edc81f650
Matthew Loseke <mloseke@gmail.com>
2014-07-17 14:35:00 -0500

    adds afterSave hook for SMSCode

---
+++ cloud/smsCode.js
@@ -0,0 +1,20 @@
+/*
+  After save we want to check and see if verified is true. If it is it means
+  the code's been sent back by a phone and we no longer need the instance around.
+  These objects are not associated with any other objects so we just blast'em
+  away.
+*/
+
+Parse.Cloud.afterSave("SMSCode", function(request) {
+  query = new Parse.Query("SMSCode");
+  query.get(request.object.get("smsCode").id, {
+    success: function(smsCode) {
+      if (smsCode.get("verified")) {
+        smsCode.delete();
+      }
+    },
+    error: function(error) {
+      console.error("Got an error " + error.code + " : " + error.message);
+    }
+  });
+});
