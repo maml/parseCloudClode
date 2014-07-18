2d3a016a4b61dbbd81e68b2d61b2a1ceec6f1a4b
Matthew Loseke <mloseke@gmail.com>
2014-07-18 17:12:22 -0500

    promisize some steps in the mentions flow

--- cloud/mentions.js
+++ cloud/mentions.js
@@ -1,34 +1,62 @@
+var mentioner;
+
 exports.extractMentions = function(request, response) {
+
+  // blurb user's objectId
+  var blurbAuthorUserId = request.object.get("user").id;
 
   // match mentions from blurb's title
   var blurbTitle = request.object.get("title");
   var mentionedUsers = blurbTitle.match(/@\w+/g);
 
   // remove the @ from each mentioned user
-  mentionedUsers = mentionedUsers.map(function(user) {
+  mentionedUsersNames = mentionedUsers.map(function(user) {
     return user.replace("\@", "");
   });
 
-  // query for the mentioned users
-  var query = new Parse.Query(Parse.User);
-  query.containedIn("username", mentionedUsers);
-
-  query.find().then(function(users) {
-    (users.length > 0) ? notify(users, response) : null;
+  getBlurbAuthorUserName(blurbAuthorUserId).then(function(user){
+    mentioner = user; // :-/
+    return getMentionedUsers(mentionedUsersNames);
+  }).then(function(users){
+    (users.length > 0) ? notify(users) : null;
+  }, function(error){
+    console.log("there was an error: " + error.code + " " + error.message);
   });
-
 }
 
-function notify(mentionedUsers) {
+// so we can have a message that goes like, "@so-and-so mentioned you . . ."
+function getBlurbAuthorUserName(id) {
+  var promise = new Parse.Promise();
+  var query = new Parse.Query(Parse.User);
+  query.get(id).then(function(user){
+    promise.resolve(user);
+  });
+  return promise;
+}
 
-  // send push notification to each user letting them know they were mentioned in a blurb
+// returns the objectified users that were mentioned
+function getMentionedUsers(mentionedUsersNames) {
+  var promise = new Parse.Promise();
+  var query = new Parse.Query(Parse.User);
+  query.containedIn("username", mentionedUsersNames);
+  query.find().then(function(users) {
+    promise.resolve(users);
+  });
+  return promise;
+}
+
+// notifications should be broken out into their own module
+function notify(users) {
+
+  var message = "@" + mentioner.get("username") + " mentioned you in a blurb."
+
   var query = new Parse.Query(Parse.Installation);
-  query.containedIn('user', mentionedUsers);
+  query.containedIn('user', users);
 
   Parse.Push.send({
     where: query,
     data: {
-      alert: "(test push) you were mentioned in a blurb",
+      alert: message,
       badge: "Increment",
       sound: "push-notification.aiff"
     }
