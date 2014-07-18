4c7c5b9d1ccecc39556bedba243445f3dd521d1b
Matthew Loseke <mloseke@gmail.com>
2014-07-18 15:45:04 -0500

    first take on pushing users when mentioned

--- cloud/mentions.js
+++ cloud/mentions.js
@@ -14,12 +14,28 @@ exports.extractMentions = function(request, response) {
   query.containedIn("username", mentionedUsers);
 
   query.find().then(function(users) {
-    (users.length > 0) ? notify(users) : null;
+    (users.length > 0) ? notify(users, response) : null;
   });
 
 }
 
-function notify(users) {
+function notify(mentionedUsers) {
+
   // send push notification to each user letting them know they were mentioned in a blurb
-  console.log("send push to users");
+  var query = new Parse.Query(Parse.Installation);
+  query.containedIn('user', mentionedUsers);
+
+  Parse.Push.send({
+    where: query,
+    data: {
+      alert: "(test push) you were mentioned in a blurb",
+      badge: "Increment",
+      sound: "push-notification.aiff"
+    }
+  }).then(function() {
+    console.log("push was successful");
+  }, function(error) {
+    console.log("there was an error sending the push: " + error.code + " " + error.message);
+  });
+
 }
